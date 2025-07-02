c5f0cd2b635c4535f732562ddff0e1ba008e1526
pranisorkruzz <kruzzthedon@gmail.com>
2025-07-02 09:14:54 +0545

    Add project files.

---
+++ math/Menu.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math
+{
+    internal class Menu
+    {
+        void Menu(string name)
+        {
+            do
+            {
+                Console.WriteLine($"hello {name} it is {date} right now and this is a math game");
+                Console.WriteLine("Please select the mode to paly:");
+                Console.WriteLine("");
+                Console.WriteLine("v.VIEW GAME HISTORY");
+                Console.WriteLine("a.ADDTION");
+                Console.WriteLine("b.SUBSTRACTION");
+                Console.WriteLine("c.MULTIPLICATION");
+                Console.WriteLine("d.DIVISION");
+                Console.WriteLine("q.QUIT THE PROGRAM");
+
+                Console.WriteLine("--------------------------------");
+
+                var gameSelected = Console.ReadLine();
+
+                switch (gameSelected.Trim().ToLower())
+                {
+                    case "v":
+                        getGames();
+                        break;
+                    case "a":
+                        additionGame("addition game selected");
+                        break;
+
+                    case "b":
+                        substractionGame("substraction game selected");
+                        break;
+
+                    case "c":
+                        multiplicationGame("multipication game selected");
+                        break;
+
+                    case "d":
+                        DivsionGame("division game selected");
+                        break;
+
+                    case "q":
+                        Console.WriteLine("Exiting..............");
+                        isGameOn = false;
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+
+                }
+            } while (isGameOn);
+        }
+
+    }
+}
---
+++ math/Program.cs
@@ -0,0 +1,187 @@
+﻿using System.ComponentModel.Design;
+using System.Numerics;
+using System.Xml.Linq;
+using math;
+
+var games = new List<string>();
+bool isGameOn = true;
+var date = DateTime.Now;
+
+var menu = new Menu();
+string name = getName();
+Menu(name);
+
+string getName()
+{
+    Console.WriteLine("enter your name:");
+    var name = Console.ReadLine();
+    return name;
+}
+
+
+void getGames()
+{
+    Console.WriteLine("------------------");
+    Console.WriteLine("Game history:");
+    foreach(var game in games)
+    {
+        Console.WriteLine(game);
+    }
+    Console.WriteLine("press any key to continue");
+    Console.ReadKey();
+    Console.Clear();
+}
+
+void additionGame(string message)
+{
+    Console.WriteLine(message);
+
+    var random = new Random();
+    int firstNumber;
+    int secondNumber;
+    int i;
+    int score = 0;
+
+    for (i = 1; i <= 5; i++)
+    {
+        firstNumber = random.Next(1, 99);
+        secondNumber = random.Next(1, 99);
+
+        Console.WriteLine($"{firstNumber} + {secondNumber} :");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber + secondNumber)
+        {
+            Console.WriteLine("congrats u got it right");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("wrong answer");
+        }
+        Console.WriteLine("press any key to continue...");
+        Console.ReadKey();
+        Console.Clear();
+    }
+    Console.WriteLine($"the game is over your final score is : {score}");
+    Console.ReadKey();
+
+    games.Add($"{DateTime.Now}-Addition score: {score}");
+}
+
+
+
+void substractionGame(string message)
+{
+    Console.WriteLine(message);
+
+    var random = new Random();
+    int firstNumber;
+    int secondNumber;
+    int i;
+    int score = 0;
+
+    for (i = 1; i <= 5; i++)
+    {
+        firstNumber = random.Next(1, 99);
+        secondNumber = random.Next(1, 99);
+
+        Console.WriteLine($"{firstNumber} - {secondNumber} :");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber - secondNumber)
+        {
+            Console.WriteLine("congrats u got it right");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("wrong answer");
+        }
+        Console.WriteLine("press any key to continue...");
+        Console.ReadKey();
+        Console.Clear();
+    }
+    Console.WriteLine($"the game is over your final score is : {score}");
+    Console.ReadKey();
+    games.Add($"{DateTime.Now}-substraction score: {score}");
+
+}
+void multiplicationGame(string message)
+{
+    Console.WriteLine(message);
+
+    var random = new Random();
+    int firstNumber;
+    int secondNumber;
+    int i;
+    int score = 0;
+
+    for (i = 1; i <= 5; i++)
+    {
+        firstNumber = random.Next(1, 99);
+        secondNumber = random.Next(1, 99);
+
+        Console.WriteLine($"{firstNumber} * {secondNumber} :");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber * secondNumber)
+        {
+            Console.WriteLine("congrats u got it right");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("wrong answer");
+        }
+        Console.WriteLine("press any key to continue...");
+        Console.ReadKey();
+        Console.Clear();
+    }
+    Console.WriteLine($"the game is over your final score is : {score}");
+    Console.ReadKey();
+    games.Add($"{DateTime.Now}-substraction score: {score}");
+}
+void DivsionGame(string message)
+{
+    Console.WriteLine(message);
+
+    var random = new Random();
+    int firstNumber;
+    int secondNumber;
+    int i;
+    int score = 0;
+
+    for (i = 1; i <= 5; i++)
+    {
+
+        do
+        {
+            firstNumber = random.Next(1, 99);
+            secondNumber = random.Next(1, 99);
+        } while (firstNumber % secondNumber != 0);
+
+
+        Console.WriteLine($"{firstNumber} / {secondNumber} :");
+        var result = Console.ReadLine();
+
+
+        if (int.Parse(result) == firstNumber / secondNumber)
+        {
+            Console.WriteLine("congrats u got it right");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("wrong answer");
+        }
+        Console.WriteLine("press any key to continue...");
+        Console.ReadKey();
+        Console.Clear();
+    }
+    Console.WriteLine($"the game is over your final score is : {score}");
+    Console.ReadKey();
+    games.Add($"{DateTime.Now}-substraction score: {score}");
+}
+
+
